0152cc7ac80f8e15193337403589f95cc943d18c
sathyavikas <69130833+sathyavikas@users.noreply.github.com>
2020-11-15 08:56:43 +0530

    Create functions.h

---
+++ implementation/inc/functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+void add_consumer();
+void output();
+void whitecalc();
+void greencalc();
+void read();
+void billing();
+void pricechange();
+int main();
+#endif
